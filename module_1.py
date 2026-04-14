96e2340ac37a1caf0d449c4ebc54c36f197a02e6
Peter Kester <aimienohopeterosagie@gmail.com>
2026-04-14 16:52:35 +0100

    Module 1 Graduation
    Finished Module 1. Ready to go deeper with Python inputs and logic!

---
+++ module_1.py
@@ -0,0 +1,14 @@
+# Day 2: Module 1 completed
+print("Hello, my name is Peter")
+print("I am learning Python")
+print("This is my second GitHub upload")
+
+beans = 500
+rice = 1000
+total = beans + rice
+print("Total cost:", total)
+
+my_name = "Peter Kester"
+level = 1
+print(my_name)
+print("I have finished module 1:", level)
