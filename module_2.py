5824d48ad00c4c0ad52e52b79292fe6e3c8d941e
Peter Kester <aimienohopeterosagie@gmail.com>
2026-04-15 22:55:05 +0100

    Day 3: Fixed my first SyntaxError
    Fixed SyntaxError and learned to use input()

---
+++ module_2.py
@@ -0,0 +1,14 @@
+# Day 3: I learned how to fix bugs!
+# Also learned how to get info from a user
+
+# 1. Using a comment and snake_case
+my_city = "Port Harcourt"
+
+# 2. Getting user input (The easy way)
+feeling = input("How is the coding going? ")
+
+# 3. Printing it all out
+print("Location: " + my_city)
+print("Status: " + feeling)
+print("Lessons finished: 2")
+
